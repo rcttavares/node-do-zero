9eea1d135ca0335ccc3f4278a1e81243d9f1cb1d
Rafael Tavares <rafael.tawares@gmail.com>
2023-10-03 18:02:50 -0300

    Add CRUD (Create, Read, Update, Delete)

--- server.js
+++ server.js
@@ -1,9 +1,52 @@
+import { DatabaseMemory } from './database-memory.js';
 import { fastify } from 'fastify';
 
+const database = new DatabaseMemory();
 const server = fastify();
 
-server.get('/', () => {
-  return 'Hello World!';
+// POST http://localhost:3333/videos
+server.post('/videos', (request, response) => {
+  const { title, description, duration } = request.body;
+
+  database.create({
+    title,
+    description,
+    duration,
+  });
+
+  return response.status(201).send();
+});
+
+// GET http://localhost:3333/videos
+server.get('/videos', (request) => {
+  const search = request.query.search;
+
+  const videos = database.list(search);
+
+  return videos;
+});
+
+// PUT http://localhost:3333/videos/3
+server.put('/videos/:id', (request, response) => {
+  const videoId = request.params.id;
+  const { title, description, duration } = request.body;
+
+  database.update(videoId, {
+    title,
+    description,
+    duration,
+  });
+
+  return response.status(204).send();
+});
+
+// DELETE http://localhost:3333/videos/3
+server.delete('/videos/:id', (request, response) => {
+  const videoId = request.params.id;
+
+  database.delete(videoId);
+
+  return response.status(204).send();
 });
 
 server.listen({
