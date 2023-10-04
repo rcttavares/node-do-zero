0fe20f46aad555a9cd83fd564c7bde63f1188b2a
Rafael Tavares <rafael.tawares@gmail.com>
2023-10-04 18:30:15 -0300

    Create Neon database with PostgresJS

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
-import { DatabaseMemory } from './database-memory.js';
+// import { DatabaseMemory } from './database-memory.js';
+import { DatabasePostgres } from './database-postgres.js';
 import { fastify } from 'fastify';
 
-const database = new DatabaseMemory();
+// const database = new DatabaseMemory();
+const database = new DatabasePostgres();
 const server = fastify();
 
 // POST http://localhost:3333/videos
-server.post('/videos', (request, response) => {
+server.post('/videos', async (request, response) => {
   const { title, description, duration } = request.body;
 
-  database.create({
+  await database.create({
     title,
     description,
     duration,
@@ -18,20 +20,20 @@ server.post('/videos', (request, response) => {
 });
 
 // GET http://localhost:3333/videos
-server.get('/videos', (request) => {
+server.get('/videos', async (request) => {
   const search = request.query.search;
 
-  const videos = database.list(search);
+  const videos = await database.list(search);
 
   return videos;
 });
 
 // PUT http://localhost:3333/videos/3
-server.put('/videos/:id', (request, response) => {
+server.put('/videos/:id', async (request, response) => {
   const videoId = request.params.id;
   const { title, description, duration } = request.body;
 
-  database.update(videoId, {
+  await database.update(videoId, {
     title,
     description,
     duration,
@@ -41,10 +43,10 @@ server.put('/videos/:id', (request, response) => {
 });
 
 // DELETE http://localhost:3333/videos/3
-server.delete('/videos/:id', (request, response) => {
+server.delete('/videos/:id', async (request, response) => {
   const videoId = request.params.id;
 
-  database.delete(videoId);
+  await database.delete(videoId);
 
   return response.status(204).send();
 });
